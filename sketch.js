87e2051d8464b59e2bd924b6460291fd845fe206
parknahyun1 <101611328+parknahyun1@users.noreply.github.com>
2022-04-05 13:08:27 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(255);
+}
+
+function draw() {
+  let r= map(mouseX,0,windowWidth,0,255);
+  let b= map(mouseX,0,windowWidth,0,255);
+  fill(r,0,b,20);
+  stroke(r/2,0,b/2,30);
+  ellipse(mouseX,mouseY,150,150);
+}
+function windowResized(){
+ reesizeCanvas(windowWidth,windowHeight); 
+}
